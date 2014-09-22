4508da2a69027b4c09429d2a7d0b767834f89037
SIVLEOL <ejlo@ualberta.ca>
2014-09-22 08:52:36 -0600

    Small update to ToDoList class

--- src/ca/ualberta/cs/ejlo/todolistforandroid/ToDoList.java
+++ src/ca/ualberta/cs/ejlo/todolistforandroid/ToDoList.java
@@ -6,27 +6,37 @@ import java.util.Collection;
 public class ToDoList {
 
 	protected ArrayList<ToDoItem> toDoList;
+	private int checkedCount = 0;
+	private int totalCount = 0;
 	
 	public ToDoList(){
 		toDoList = new ArrayList<ToDoItem>();
 	}
-	
-	public Collection getToDoItems() {
+	public Collection<ToDoItem> getToDoItems() {
 		return toDoList;
 	}
-
 	public void addItem(ToDoItem Item) {
 		toDoList.add(Item);
 	}
-
 	public void removeItem(ToDoItem Item) {
 		toDoList.remove(Item);
 	}
-	
 	public ToDoItem popItem(ToDoItem Item){
 		ToDoItem tempItem = Item;
 		toDoList.remove(Item);
 		return tempItem;
+	}
+	public int getCheckedCount(){
+		return checkedCount;
+	}
+	public int getTotalCount(){
+		return totalCount;
+	}
+	public void setCheckedCount(int count){
+		checkedCount = count;
+	}
+	public void setTotalCount(int count){
+		totalCount = count;
 	}
 
 }
