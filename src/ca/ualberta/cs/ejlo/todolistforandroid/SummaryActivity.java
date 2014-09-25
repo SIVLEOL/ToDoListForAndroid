4dd2664466dba67f7f4d4121a02a5adcb61c6cb2
SIVLEOL <ejlo@ualberta.ca>
2014-09-25 16:45:49 -0600

    Summary activity done, email + misc left to do

--- src/ca/ualberta/cs/ejlo/todolistforandroid/SummaryActivity.java
+++ src/ca/ualberta/cs/ejlo/todolistforandroid/SummaryActivity.java
@@ -1,18 +1,65 @@
 package ca.ualberta.cs.ejlo.todolistforandroid;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SummaryActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_summary);
 		
 		ToDoListManager.initManager(this.getApplicationContext());
+		
+		int toDoLen;
+		int archiveLen;
+		int toDoCheckCount = 0;
+		int archiveCheckCount = 0;
+		
+		TextView toDoSummary = (TextView) findViewById(R.id.toDoSummary);
+		TextView toDoChecked = (TextView) findViewById(R.id.toDoChecked);
+		TextView toDoNotChecked = (TextView) findViewById(R.id.toDoNotChecked);
+		TextView archiveSummary = (TextView) findViewById(R.id.archiveSummary);
+		TextView archiveChecked = (TextView) findViewById(R.id.archiveChecked);
+		TextView archiveNotChecked = (TextView) findViewById(R.id.archiveNotChecked);
+		TextView totalSummary = (TextView) findViewById(R.id.totalSummary);
+		
+		Collection<ToDoItem> tempList = ToDoListController.getToDoList().getToDoItems();
+        ArrayList<ToDoItem> toDoList = new ArrayList<ToDoItem>(tempList);
+        
+        Collection<ToDoItem> tempList2 = ToDoListController.getToDoList().getArchiveItems();
+        ArrayList<ToDoItem> archiveList = new ArrayList<ToDoItem>(tempList2);
+        
+        toDoLen = toDoList.size();
+        archiveLen = archiveList.size();
+        
+        for (int i = 0; i < toDoLen; i++){
+			if (toDoList.get(i).getCheck() == 1){
+				toDoCheckCount = toDoCheckCount + 1;
+			}
+		}
+        
+        for (int i = 0; i < archiveLen; i++){
+			if (archiveList.get(i).getCheck() == 1){
+				archiveCheckCount = archiveCheckCount + 1;
+			}
+		}
+        
+        toDoSummary.setText("To Do Items: " + toDoLen);
+        toDoChecked.setText("Checked: " + toDoCheckCount);
+        toDoNotChecked.setText("Unchecked: " + (toDoLen - toDoCheckCount));
+        archiveSummary.setText("Archived Items: " + archiveLen);
+        archiveChecked.setText("Checked: " + archiveCheckCount);
+        archiveNotChecked.setText("Unchecked: " + (archiveLen - archiveCheckCount));
+        totalSummary.setText("Total Items: " + (toDoLen + archiveLen));
+        
 	}
 
 	@Override
